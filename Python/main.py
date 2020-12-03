1e452b0d7dbb3576fd4078d901c9d95bbfa1f413
Ram Lugo <ramlugo2020@gmail.com>
2020-12-02 20:52:33 -0600

    Primeros objetos de la clase car

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola mundo")
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andrés Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Martha Nielsen"
+    print(vars(car2))
